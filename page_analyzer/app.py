068a62bf4eecf376c20c098e76b3d33605bf8ae9
Arina Zubova <arina777pototskaya@gmail.com>
2023-01-30 14:21:49 +0400

    refactor check_url

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,3 @@
-from datetime import date
 import secrets
 import requests
 import psycopg2
@@ -117,8 +116,8 @@ def check_url(id):
     soup = BeautifulSoup(html_content, 'html.parser')
     h1 = soup.h1.text if soup.find('h1') else " "
     title = soup.title.text if soup.find('title') else " "
-    description = soup.find("meta", attrs={"name": "description"})
-    description = f'{description.get("content")[:255]}...' if description else " "
+    descript = soup.find("meta", attrs={"name": "description"})
+    description = f'{descript.get("content")[:255]}...' if descript else " "
     cur.execute('''
         INSERT INTO url_checks (url_id, status_code, h1, title, description)
         VALUES (%s, %s, %s, %s, %s)''',
