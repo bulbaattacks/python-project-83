40b11f42c5bbd5b0d71ea1fd3cbb767dbdf0d068
Arina Zubova <arina777pototskaya@gmail.com>
2023-02-14 14:56:33 +0400

    refacrot app with conn

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -29,20 +29,21 @@ def index():
 @app.get('/urls')
 def get_urls():
     conn = get_conn()
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        curs.execute(
-            '''
-            SELECT urls.id, urls.name, url_checks.status_code,
-            DATE(url_checks.created_at)
-            FROM urls
-            LEFT JOIN url_checks on urls.id=url_checks.url_id
-            AND url_checks.created_at =
-                (SELECT MAX(created_at)
-                    FROM url_checks
-                    WHERE url_id = urls.id)
-            ORDER BY urls.id DESC
-            ''')
-        result = curs.fetchall()
+    with conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as curs:
+            curs.execute(
+                '''
+                SELECT urls.id, urls.name, url_checks.status_code,
+                DATE(url_checks.created_at)
+                FROM urls
+                LEFT JOIN url_checks on urls.id=url_checks.url_id
+                AND url_checks.created_at =
+                    (SELECT MAX(created_at)
+                        FROM url_checks
+                        WHERE url_id = urls.id)
+                ORDER BY urls.id DESC
+                ''')
+            result = curs.fetchall()
     conn.close()
     return render_template('all_urls.html', all_urls=result)
 
@@ -54,21 +55,21 @@ def add_url():
     if errors:
         return render_template('index.html', not_correct_data=data), 422
     conn = get_conn()
-    with conn.cursor() as curs:
-        curs.execute('SELECT * FROM urls WHERE urls.name = %s', (data,))
-        result = curs.fetchone()
-    conn.commit()
-    if not result:
+    with conn:
         with conn.cursor() as curs:
-            curs.execute('INSERT INTO urls (name) VALUES (%s)', (data,))
-            curs.execute('SELECT id FROM urls WHERE urls.name = %s', (data,))
-            id = curs.fetchone()[0]
+            curs.execute('SELECT * FROM urls WHERE urls.name = %s', (data,))
+            result = curs.fetchone()
         conn.commit()
-        flash("Страница успешно добавлена", "success")
-        conn.close()
-        return redirect(url_for("show_url", id=id))
-    id = result[0]
-    flash("Страница уже существует", "info")
+        if not result:
+            with conn.cursor() as curs:
+                curs.execute('INSERT INTO urls (name) VALUES (%s)', (data,))
+                curs.execute('SELECT id FROM urls WHERE urls.name = %s', (data,))
+                id = curs.fetchone()[0]
+            conn.commit()
+            flash("Страница успешно добавлена", "success")
+            return redirect(url_for("show_url", id=id))
+        id = result[0]
+        flash("Страница уже существует", "info")
     conn.close()
     return redirect(url_for("show_url", id=id))
 
@@ -76,24 +77,25 @@ def add_url():
 @app.get('/urls/<int:id>')
 def show_url(id):
     conn = get_conn()
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        curs.execute('''
-                SELECT id, name, DATE(created_at) as created_at
-                FROM urls WHERE urls.id = %s
-                ''', (id,))
-        result = curs.fetchone()
-    if not result:
-        curs.close()
-        conn.close()
-        return render_template('404.html'), 404
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        curs.execute('''
-            SELECT id, status_code, h1,
-            title, description, DATE(created_at) as created_at, url_id
-            FROM url_checks
-            WHERE url_checks.url_id = %s
-            ORDER BY id DESC''', (id,))
-        check = curs.fetchall()
+    with conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as curs:
+            curs.execute('''
+                    SELECT id, name, DATE(created_at) as created_at
+                    FROM urls WHERE urls.id = %s
+                    ''', (id,))
+            result = curs.fetchone()
+        if not result:
+            curs.close()
+            conn.close()
+            return render_template('404.html'), 404
+        with conn.cursor(cursor_factory=RealDictCursor) as curs:
+            curs.execute('''
+                SELECT id, status_code, h1,
+                title, description, DATE(created_at) as created_at, url_id
+                FROM url_checks
+                WHERE url_checks.url_id = %s
+                ORDER BY id DESC''', (id,))
+            check = curs.fetchall()
     conn.commit()
     conn.close()
     return render_template('show.html', check_url=check, show_url=result)
@@ -102,24 +104,25 @@ def show_url(id):
 @app.post('/urls/<id>/checks')
 def check_url(id):
     conn = get_conn()
-    with conn.cursor() as curs:
-        curs.execute('SELECT name FROM urls WHERE urls.id = %s', (id,))
-        name = curs.fetchone()[0]
-    try:
-        parser = prepare_seo_data(name)
-        status_code = parser.get('status_code')
-        h1 = parser.get('h1')
-        title = parser.get('title')
-        description = parser.get('description')
+    with conn:
         with conn.cursor() as curs:
-            curs.execute('''
-                INSERT INTO url_checks
-                (url_id, status_code, h1, title, description)
-                VALUES (%s, %s, %s, %s, %s)''',
-                         (id, status_code, h1, title, description,))
-        conn.commit()
-        flash("Страница успешно проверена", "success")
-    except Exception:
-        flash("Произошла ошибка при проверке", "danger")
+            curs.execute('SELECT name FROM urls WHERE urls.id = %s', (id,))
+            name = curs.fetchone()[0]
+        try:
+            parser = prepare_seo_data(name)
+            status_code = parser.get('status_code')
+            h1 = parser.get('h1')
+            title = parser.get('title')
+            description = parser.get('description')
+            with conn.cursor() as curs:
+                curs.execute('''
+                    INSERT INTO url_checks
+                    (url_id, status_code, h1, title, description)
+                    VALUES (%s, %s, %s, %s, %s)''',
+                             (id, status_code, h1, title, description,))
+            conn.commit()
+            flash("Страница успешно проверена", "success")
+        except Exception:
+            flash("Произошла ошибка при проверке", "danger")
     conn.close()
     return redirect(url_for('show_url', id=id))
